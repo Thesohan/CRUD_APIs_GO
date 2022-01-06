a48f43b0f8441e8c5f10e19f04136efd56f95970
Thesohan <sohan.cse16@nituk.ac.in>
2022-01-06 23:14:29 +0530

    GO CRUD APIs

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/first_api/Config"
+	"github.com/first_api/Models"
+	"github.com/first_api/Routes"
+	"github.com/jinzhu/gorm"
+)
+
+var err error
+
+func main() {
+	Config.DB, err = gorm.Open("mysql", Config.DbURL(Config.BuildDBConfig()))
+	if err != nil {
+		fmt.Println("Status:", err)
+	}
+	defer Config.DB.Close()
+	Config.DB.AutoMigrate(&Models.User{})
+	r := Routes.SetupRouter()
+	//running
+	r.Run()
+}
